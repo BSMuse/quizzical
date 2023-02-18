db524270b0bb9a1495558f8814bd8d16acbaddf7
bsmuse <bsmuse@tutamail.com>
2023-02-17 17:02:26 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ function App() {
   const [displayStart, changeStart] = React.useState(true) 
   const [displayQuiz, changeQuiz] = React.useState(false)
   const [questions, changeQuestions] = React.useState([])
+  const allQuestions = ""
 
   React.useEffect(()=> {
-    fetch(`https://opentdb.com/api.php?amount=4&difficulty=hard&type=multiple`)
+    fetch(`https://opentdb.com/api.php?amount=4&difficulty=easy&type=multiple`)
     .then(res => res.json())
     .then(data => {changeQuestions(data.results)})
   }, [0])
@@ -18,53 +19,7 @@ function App() {
 const handleClickToStart = ()=> {
   changeStart(false)
   changeQuiz(true)
-  // console.log(questions[0].incorrect_answers)
-  const test = allQuestions(questions[0])
-  console.log(test[0])
 }
-
-
-
-function addArrays(array1, array2) {
-  let array3 = array2.concat(array1)
-  let array4 = []
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = Math.floor(Math.random() * array3.length)
-    const element = array3.splice([randomIndex],1)[0]
-    array4.push(element)
-  }
-  return array4;
-}
-
-
-class Question {
-  constructor(questions){
-    Object.assign(this, questions)
-  }
-  newObject() 
-  {
-    const { question, correct_answer, incorrect_answers} = this
-    return (
-      // `This is the question:${question} this is the right answer ${correct_answer} and these are wrong ${incorrect_answers}`
-      {aQuestion: question, 
-      answers: addArrays(correct_answer, incorrect_answers),
-      correct: correct_answer,}
-    )
-  }s
-}
-
-const allQuestions = (array) => {
-  let questionsArray = ""
-for (let i=0; i <array.length; i++) {
-  questionsArray += new Question(array[i])
-}
-return questionsArray
-}
-
-
-// const handleClickAnswerCheck = ()=> {
-
-// } 
 
   return (
     <div className="App">
@@ -73,6 +28,7 @@ return questionsArray
       styles={!displayStart ? "none" : "block" }
       />
        <Quiz
+       questions={questions}
       styles={displayQuiz ? "block" : "none" }
        /> 
     </div>
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -3,17 +3,72 @@ import './Quiz.css'
 
 export default function Quiz(props) {
     const styles = {display: props.styles}
-    return (
-    <div className='Quiz' style={styles}>
-        <div className='quiz-start'>
+    const { questions } = props
+
+    const handleClick = (event)=> {
+        const parent = event.target.parentElement
+        const childElements = parent.querySelectorAll("button")
+        childElements.forEach((childElement) => {
+            childElement.style.backgroundColor = "#F5F7FB"
+            childElement.classNameList.remove = "selected"
+            
+    })
+        event.target.style.backgroundColor="#D6DBF5"
+        event.target.classNameList.add = "selected"
+    }
+
+     const checkAnswers = () => {
+        const buttons = document.querySelectorAll("button")
+        buttons.forEach((button) => {
+            if (button.className === "correct") {
+                button.style.backgroundColor = "#94D7A2"
+            } else if (button.className === "selected"){
+                button.style.backgroundColor = "red"
+            }else {
+                button.style.opacity = "0.5"
+            }
+    })
+}
+
+    function decodeHtml(html) {
+        const txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+      }
+
+    function addArrays(array1, array2) {
+        let array3 = array2.concat(array1)
+        let array4 = []
+        for (let i = 0; i < 4; i++) {
+          const randomIndex = Math.floor(Math.random() * array3.length)
+          const element = array3.splice([randomIndex],1)[0]
+          array4.push(element)
+        }
+        return array4;
+      }
+      
+      const allQuestions = questions.map(item => {
+        const { question, correct_answer, incorrect_answers } = item
+          const answers = addArrays(correct_answer, incorrect_answers)
+          return (
             <div className='quiz-question'>
-                {/* <h3>{props[0].question}</h3> */}
-                <div className='answers'>
-                    <button></button><button></button><button></button><button></button>
-                </div>
+                      <p>{decodeHtml(question)}</p>
+                      <div className='answers'>
+                          <button onClick={handleClick} className={correct_answer === answers[0] ?  "correct" : "incorrect" }>{decodeHtml(answers[0])}</button>
+                          <button onClick={handleClick} className={correct_answer === answers[1] ?  "correct" : "incorrect" }>{decodeHtml(answers[1])}</button>
+                          <button onClick={handleClick} className={correct_answer === answers[2] ?  "correct" : "incorrect" }>{decodeHtml(answers[2])}</button>
+                          <button onClick={handleClick} className={correct_answer === answers[3] ?  "correct" : "incorrect" }>{decodeHtml(answers[3])}</button>
+                      </div>
             </div>
-            <button className='check-answers'>Check answers</button>
+          )
+      })
+
+    return (
+    <div className='quiz' style={styles}>
+        <div className='quiz-start'>
+            {allQuestions}
         </div>
+            <button className='check-answers' onClick={checkAnswers}>Check answers</button>
     </div>
     )
 }

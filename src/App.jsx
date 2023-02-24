b49d1ef6cb15f08e943cd8aff60ebc29dc5e3268
bsmuse <bsmuse@tutamail.com>
2023-02-24 10:09:52 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,29 @@ function App() {
   const [displayStart, changeStart] = React.useState(true) 
   const [displayQuiz, changeQuiz] = React.useState(false)
   const [questions, changeQuestions] = React.useState([])
-  const allQuestions = ""
+
+  function addArrays(array1, array2) {
+    let array3 = array2.concat(array1)
+    let array4 = []
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * array3.length)
+      const element = array3.splice([randomIndex],1)[0]
+      array4.push(element)
+    }
+    return array4;
+  }
+
 
   React.useEffect(()=> {
     fetch(`https://opentdb.com/api.php?amount=4&difficulty=easy&type=multiple`)
     .then(res => res.json())
-    .then(data => {changeQuestions(data.results)})
-  }, [0])
+    .then(data => {
+      const newQuestion = data.results
+      changeQuestions(newQuestion.map(question => {
+        return {...question, answer: addArrays(question.correct_answer,question.incorrect_answers)}
+      }))
+    })
+  }, [])
 
 const handleClickToStart = ()=> {
   changeStart(false)
@@ -28,6 +44,7 @@ const handleClickToStart = ()=> {
       styles={!displayStart ? "none" : "block" }
       />
        <Quiz
+       displayQuiz = {displayQuiz}
        questions={questions}
       styles={displayQuiz ? "block" : "none" }
        /> 
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,74 +1,96 @@
 import React from 'react'
 import './Quiz.css'
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 export default function Quiz(props) {
+    const [allQuestions, changeQuestions] = React.useState()
+    const [quiz,quizDone] = React.useState(false) 
+    let ids = []
+    const [score, changeScore] = React.useState(0)
+    const [chosenIds, changeIds] = React.useState([])
     const styles = {display: props.styles}
     const { questions } = props
+    const { displayQuiz } = props
 
     const handleClick = (event)=> {
+        let questionIds = []
         const parent = event.target.parentElement
         const childElements = parent.querySelectorAll("button")
         childElements.forEach((childElement) => {
-            childElement.style.backgroundColor = "#F5F7FB"
-            childElement.classNameList.remove = "selected"
-            
+            childElement.style.backgroundColor = "#F5F7FB" 
+            questionIds.push(childElement.id)
     })
-        event.target.style.backgroundColor="#D6DBF5"
-        event.target.classNameList.add = "selected"
+        event.target.style.backgroundColor= "#D6DBF5"
+        ids = ids.filter(item => !questionIds.includes(item))
+        ids.push(event.target.getAttribute('id'))
+        changeIds(ids)
     }
 
      const checkAnswers = () => {
+        quizDone(true)
         const buttons = document.querySelectorAll("button")
         buttons.forEach((button) => {
-            if (button.className === "correct") {
+            if (chosenIds.includes(button.id) && button.className === "correct") {
                 button.style.backgroundColor = "#94D7A2"
-            } else if (button.className === "selected"){
-                button.style.backgroundColor = "red"
-            }else {
+                changeScore(prevState=>prevState + 1)
+            }else if (button.className === "correct"){
+                button.style.backgroundColor = "#94D7A2"
+            } 
+            else if (button.className === "incorrect" && chosenIds.includes(button.id)) {
+                button.style.backgroundColor = "#FF0000"
+            }
+            else {
                 button.style.opacity = "0.5"
             }
     })
 }
 
     function decodeHtml(html) {
-        const txt = document.createElement("textarea");
-        txt.innerHTML = html;
-        return txt.value;
+        const txt = document.createElement("textarea")
+        txt.innerHTML = html
+        return txt.value
       }
 
-    function addArrays(array1, array2) {
-        let array3 = array2.concat(array1)
-        let array4 = []
-        for (let i = 0; i < 4; i++) {
-          const randomIndex = Math.floor(Math.random() * array3.length)
-          const element = array3.splice([randomIndex],1)[0]
-          array4.push(element)
-        }
-        return array4;
-      }
-      
-      const allQuestions = questions.map(item => {
-        const { question, correct_answer, incorrect_answers } = item
-          const answers = addArrays(correct_answer, incorrect_answers)
+
+      React.useEffect(()=> {changeQuestions(
+        questions.map(item => {
+        const { question, correct_answer, answer} = item
           return (
             <div className='quiz-question'>
                       <p>{decodeHtml(question)}</p>
                       <div className='answers'>
-                          <button onClick={handleClick} className={correct_answer === answers[0] ?  "correct" : "incorrect" }>{decodeHtml(answers[0])}</button>
-                          <button onClick={handleClick} className={correct_answer === answers[1] ?  "correct" : "incorrect" }>{decodeHtml(answers[1])}</button>
-                          <button onClick={handleClick} className={correct_answer === answers[2] ?  "correct" : "incorrect" }>{decodeHtml(answers[2])}</button>
-                          <button onClick={handleClick} className={correct_answer === answers[3] ?  "correct" : "incorrect" }>{decodeHtml(answers[3])}</button>
+                          <button onClick={handleClick} id = {nanoid()} className={correct_answer === answer[0] ?  "correct" : "incorrect" }>{decodeHtml(answer[0])}</button>
+                          <button onClick={handleClick} id = {nanoid()} className={correct_answer === answer[1] ?  "correct" : "incorrect" }>{decodeHtml(answer[1])}</button>
+                          <button onClick={handleClick} id = {nanoid()} className={correct_answer === answer[2] ?  "correct" : "incorrect" }>{decodeHtml(answer[2])}</button>
+                          <button onClick={handleClick} id = {nanoid()} className={correct_answer === answer[3] ?  "correct" : "incorrect" }>{decodeHtml(answer[3])}</button>
                       </div>
             </div>
-          )
-      })
-
+            )
+         })
+        )
+        },[displayQuiz])
+        
+        const playAgain = () => {
+            console.log('clicked')
+            quizDone(false)
+            redoQuiz(true)
+        }
+        const correctAnswers = 
+        <div className='correct-container'>
+            <p>You scored {score}/4 correct answers</p>
+            <button className='play-again' onClick={playAgain}>Play again</button>
+        </div>
+    
     return (
     <div className='quiz' style={styles}>
         <div className='quiz-start'>
             {allQuestions}
         </div>
-            <button className='check-answers' onClick={checkAnswers}>Check answers</button>
+            {!quiz && <button className='check-answers' onClick={checkAnswers} disabled={chosenIds.length < 4}>Check answers</button>}
+            <div>
+            {quiz && correctAnswers}
+            {/* {redo && <Quiz/>} */}
+        </div>
     </div>
     )
 }
